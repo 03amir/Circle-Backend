0ed2a52039c0f80ac19bc3f79130d5785b91abae
Amirul Sekh <sekhemarul123@gmail.com>
2022-03-30 10:36:15 +0530

    asd

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-var cors = require('cors')
-app.use(cors())
+var cors = require("cors");
+app.use(cors());
 
-// requiring the jsn for getting data from the body this is have to be in the top level 
-app.use(express.json())
+//requiring the jsn for getting data from the body this is have to be in the top level
+app.use(express.json());
 
 const mongoose = require("mongoose");
-mongoose.connect(process.env.URI,()=>{
-    console.log("connected to the database")
-})
+mongoose.connect(process.env.URI, () => {
+  console.log("connected to the database");
+});
 
 //requiring the auth route
 const authRouter = require("./routes/aut");
@@ -18,37 +18,16 @@ app.use(authRouter);
 const postRouter = require("./routes/post");
 app.use(postRouter);
 
-//requreing the user schema
+//requreing the  schema
 const user = require("./models/userSchema");
 const post = require("./models/postSchema");
 
-
-// middle wares
-const middlewares = (req,res,next)=>{
-
-    //console.log(user)
-    next()
-
-}
-
-//connect to the database
-
-
-
-
-
 //Normal Routes
-
-app.get("/",(req,res)=>{
-    res.status(200).send("hello from the server")
-})
-
-app.get("/about",middlewares,(req,res)=>{
-    res.status(200).send("hello from the server")
-})
-
+app.get("/", (req, res) => {
+  res.status(200).send("hello from the server");
+});
 
 //Running the server on the port
-app.listen( process.env.PORT || 8000 ,()=>{
-    console.log("serveer is running on port 8000")
-})
+app.listen(process.env.PORT || 8000, () => {
+  console.log("serveer is running on port 8000");
+});
